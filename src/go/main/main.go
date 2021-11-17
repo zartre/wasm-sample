8976f804a3fe82decbaeeec4bee65c25cd66b152
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2021-11-17 20:27:49 +0700

    add isEmail func

--- src/go/main/main.go
+++ src/go/main/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"net/mail"
+	"syscall/js"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	fmt.Println("Loaded Wasm")
+	js.Global().Set("isEmail", isEmailWrapper)
+	<-make(chan bool)
 }
+
+func isEmail(s string) bool {
+	_, err := mail.ParseAddress(s)
+	return err == nil
+}
+
+var isEmailWrapper = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+	if len(args) != 1 {
+		return errors.New("requires 1 argument")
+	}
+	s := args[0].String()
+	return isEmail(s)
+})
